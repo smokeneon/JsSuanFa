8b1816d9a897f59e0d2a5d3b9a28162fb9e796c2
leon <leon@hooc.top>
2020-04-11 12:02:14 +0800

    测试

--- src/fanzhuan557.js
+++ src/fanzhuan557.js
@@ -1,3 +1,18 @@
+/**
+ * 
+ * @param {字符串} str 
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+
+示例 1:
+
+输入: "Let's take LeetCode contest"
+输出: "s'teL ekat edoCteeL tsetnoc" 
+注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-words-in-a-string-iii
+
+ */
 function fanzhuan557(str){
     return str.split(' ').map((item)=>{
         return item.split('').reverse().join('');
